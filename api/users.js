8a0f7b068dbb2de85ce30a06c494a008e2f8e850
Walter <walter18lopez@gmail.com>
2023-02-09 15:38:59 -0500

    Almost completed User routes

--- api/users.js
+++ api/users.js
@@ -3,30 +3,33 @@ const express = require("express");
 const router = express.Router();
 // require('./db');
 const jwt = require("jsonwebtoken");
-const { createUser, getUserByUsername } = require("../db");
+const { JWT_SECRET } = process.env
+const { createUser, getUserByUsername, getUser } = require("../db");
+const { UserDoesNotExistError, PasswordTooShortError, UserTakenError } = require('../errors');
 
 // POST /api/users/register
-router.post('/register', async (req, res, next)=> {
+router.post('/register', async (req, res, next) => {
     const { username, password } = req.body;
-    
+
     try {
-        const user = await createUser({username, password});   
         const _user = await getUserByUsername(username);
-        
+
         if (_user) {
-            res.send({
-                name: "duplicate user err",
-                message: `User ${_user.username} is already taken.`,
-                error: "cannot duplicate user"
-            })
-              
-        }else if(user.password.length <= 8){
-            res.send({
-                name: "duplicate password",
-                message: "Password Too Short!",
-                error: "cannot duplicate password"
+            res.status(500).send({
+                "name": username,
+                "message": UserTakenError(username),
+                "error": "DuplicateUsernameError"
             })
         }
+        if (password.length < 8) {
+            res.status(500).send({
+                "name": username,
+                "message": PasswordTooShortError(),
+                "error": "PasswordTooShortError"
+            })
+        }
+        //console.log(token);
+        const user = await createUser({ username, password });
         const token = jwt.sign(
             {
                 id: user.id,
@@ -37,23 +40,67 @@ router.post('/register', async (req, res, next)=> {
                 expiresIn: "1w",
             }
         );
-        //console.log(token);
         res.send({
             message: "thank you for signing up",
             token,
-            user, 
+            user,
         });
-        
-    } catch ( {name, message, error} ) {
-        next( {name, message, error} );
+
+    } catch ({ name, message, error }) {
+        next({ name, message, error });
     }
-    
+
 });
 
 // POST /api/users/login
+router.post('/login', async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+        const user = await getUserByUsername(username);
+        if (user.password === password) {
+            const token = jwt.sign({ id: user.id, username }, process.env.JWT_SECRET);
+            res.send({
+                message: "you're logged in!",
+                token: token,
+                user: {
+                    id: user.id,
+                    username: user.username
+                },
+            });
+        }
+    } catch (err) {
+        next(err);
+    }
 
+})
 // GET /api/users/me
+router.get('/me', async (req, res, next) => {
+    try {
+        const token = req.header('Authorization');
+        if (!token) {
+            res.status(401).send({
+                "error": "UserNotLoggedInError",
+                "message": "You must be logged in to perform this action",
+                "name": "UserDoesnotExist"
+            })
+        }
+        const newToken = token.slice(7)
+        const verify = jwt.verify(newToken, process.env.JWT_SECRET)
+        const user = await getUserByUsername(verify.username);
+        if (user) {
+            res.send(user);
+        } else {
+            next('User Not Available');
+        }
+    } catch (err) {
+        next(err);
+    }
+});
 
 // GET /api/users/:username/routines
+router.get('/:username/routines', async (req, res, next) => {
+
+});
+//TO GET ACCESS TO :USERNAME USE req.params
 
 module.exports = router;
