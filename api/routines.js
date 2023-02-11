675a2a8ec8e66e332911b9ad8bf99170a35ffd76
Walter <walter18lopez@gmail.com>
2023-02-11 15:55:22 -0500

    Almost completed routine routes, just need 3 routes to complete

--- api/routines.js
+++ api/routines.js
@@ -1,11 +1,70 @@
 const express = require('express');
+const { getAllPublicRoutines, createRoutine, updateRoutine } = require('../db');
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 
 // GET /api/routines
 
+router.get('/', async (req, res, next) => {
+    try {
+        const routines = await getAllPublicRoutines();
+        res.send(routines);
+    } catch (err) {
+        next(err);
+    }
+});
+
 // POST /api/routines
 
+router.post('/', async (req, res, next) => {
+    const { isPublic, name, goal } = req.body;
+    const token = req.header('Authorization');
+    try {
+        if (!token) {
+            res.status(401).send({
+                "error": "UserNotLoggedInError",
+                "message": "You must be logged in to perform this action",
+                "name": "UserDoesnotExist"
+            })
+        }
+        else {
+            const newToken = token.slice(7);
+            const isLoggedIn = jwt.verify(newToken, process.env.JWT_SECRET);
+            const creatorId = isLoggedIn.id;
+            const routine = await createRoutine({ creatorId, isPublic, name, goal });
+            res.send(routine)
+        }
+    } catch (err) {
+        next(err);
+    }
+});
+
 // PATCH /api/routines/:routineId
+
+router.patch('/:routineId', async (req, res, next) => {
+    const { routineId } = req.params;
+    const { isPublic, name, goal } = req.body;
+    const token = req.header('Authorization');
+    try {
+        if (!token) {
+            res.status(401).send({
+                "error": "UserNotLoggedInError",
+                "message": "You must be logged in to perform this action",
+                "name": "UserDoesnotExist"
+            })
+        } else {
+            const newToken = token.slice(7);
+            const isLoggedIn = jwt.verify(newToken, process.env.JWT_SECRET);
+            const creatorId = isLoggedIn.id;
+            const updateRoutine = await updateRoutine({ routineId, isPublic, name, goal });
+            console.log(updateRoutine)
+            res.send(updateRoutine);
+        }
+    } catch (err) {
+        next(err);
+    }
+});
 
 // DELETE /api/routines/:routineId
 
